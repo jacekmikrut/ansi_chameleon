02064fdaee4688ab00f8c23ca61a4d73876545f5
Jacek Mikrut <jacekmikrut.software@gmail.com>
2012-01-29 15:56:22 +0100

    Removed AnsiChameleon::ArrayUtils.includes_other_array_items_in_order? method.

--- lib/ansi_chameleon/array_utils.rb
+++ lib/ansi_chameleon/array_utils.rb
@@ -16,19 +16,5 @@ module AnsiChameleon
 
       remaining_items.any? ? nil : map
     end
-
-    def self.includes_other_array_items_in_order?(array, other_array)
-      return true if other_array.none?
-
-      remaining_array = array.dup
-      other_array.each_with_index do |other_array_item|
-        if array_item_index = remaining_array.index(other_array_item)
-          remaining_array.slice!(0, array_item_index)
-        else
-          return false
-        end
-      end
-      true
-    end
   end
 end
