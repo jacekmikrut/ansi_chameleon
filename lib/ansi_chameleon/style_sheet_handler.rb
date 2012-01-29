6985de25d1c1bd13be06ad1fdfa713f802cbe37a
Jacek Mikrut <jacekmikrut.software@gmail.com>
2012-01-29 16:04:56 +0100

    Now AnsiChameleon::StyleSheetHandler stores default values in an instance variable again.

--- lib/ansi_chameleon/style_sheet_handler.rb
+++ lib/ansi_chameleon/style_sheet_handler.rb
@@ -3,26 +3,17 @@ module AnsiChameleon
 
     def initialize(style_sheet_hash, property_name_translator=nil)
       @tag_names = []
+      @default_values = {}
       @map = {}
       @property_name_translator = property_name_translator
       populate_map(style_sheet_hash)
     end
 
-    attr_reader :tag_names, :map, :property_name_translator
+    attr_reader :tag_names, :default_values, :map, :property_name_translator
 
     def value_for(*tag_names_chain, property_name)
       strongest = strongest_style_definition(*(tag_names_chain.map { |tag_name| tag_name.to_sym }), property_name.to_sym)
       strongest && strongest[:value]
-    end
-
-    def default_values
-      default_values = {}
-      @map.each do |property_name, property_entries|
-        if entry_with_default_value = property_entries.find { |entry| entry[:tag_names_chain].none? }
-          default_values[property_name] = entry_with_default_value[:value]
-        end
-      end
-      default_values
     end
 
     private
@@ -42,6 +33,7 @@ module AnsiChameleon
 
           @map[property_name] ||= []
           @map[property_name] << { :value => value, :tag_names_chain => tag_names_chain }
+          @default_values[property_name] = value if tag_names_chain.none?
 
         end
       end
