896879e159e9b5dd48b9d84698cae1706fc8b634
Jacek Mikrut <jacekmikrut.software@gmail.com>
2012-01-29 15:38:27 +0100

    Implemented AnsiChameleon::ArrayUtils.item_spread_map method.

--- lib/ansi_chameleon/array_utils.rb
+++ lib/ansi_chameleon/array_utils.rb
@@ -1,6 +1,22 @@
 module AnsiChameleon
   module ArrayUtils
 
+    def self.item_spread_map(array, items)
+      remaining_items = items.dup
+
+      map = array.inject('') do |agg, array_item|
+        if array_item == remaining_items.first
+          agg << '1'
+          remaining_items.shift
+        else
+          agg << '0'
+        end
+        agg
+      end
+
+      remaining_items.any? ? nil : map
+    end
+
     def self.includes_other_array_items_in_order?(array, other_array)
       return true if other_array.none?
 
--- spec/ansi_chameleon/array_utils_spec.rb
+++ spec/ansi_chameleon/array_utils_spec.rb
@@ -2,6 +2,76 @@ require "spec_helper"
 
 describe AnsiChameleon::ArrayUtils do
 
+  describe ".item_spread_map" do
+    subject { AnsiChameleon::ArrayUtils.item_spread_map(array, other_array) }
+
+    context "([], [])" do
+      let(:array)       { [] }
+      let(:other_array) { [] }
+      it { should == "" }
+    end
+
+    context "([], [:a])" do
+      let(:array)       { [] }
+      let(:other_array) { [:a] }
+      it { should be_nil }
+    end
+
+    context "([:a], [])" do
+      let(:array)       { [:a] }
+      let(:other_array) { [  ] }
+      it { should == "0" }
+    end
+
+    context "([:a], [:a])" do
+      let(:array)       { [:a] }
+      let(:other_array) { [:a] }
+      it { should == "1" }
+    end
+
+    context "([:a, :b], [:a])" do
+      let(:array)       { [:a, :b] }
+      let(:other_array) { [:a    ] }
+      it { should == "10" }
+    end
+
+    context "([:a, :b], [:a, :b])" do
+      let(:array)       { [:a, :b] }
+      let(:other_array) { [:a, :b] }
+      it { should == "11" }
+    end
+
+    context "([:a, :b], [:b, :a])" do
+      let(:array)       { [:a, :b] }
+      let(:other_array) { [    :b, :a] }
+      it { should be_nil }
+    end
+
+    context "([:a, :b, :c, :d], [:b, :d])" do
+      let(:array)       { [:a, :b, :c, :d] }
+      let(:other_array) { [    :b,     :d] }
+      it { should == "0101" }
+    end
+
+    context "([:a, :b, :c, :d], [:a, :b, :d])" do
+      let(:array)       { [:a, :b, :c, :d] }
+      let(:other_array) { [:a, :b,     :d] }
+      it { should == "1101" }
+    end
+
+    context "([:a, :b, :c, :d], [:b, :a])" do
+      let(:array)       { [:a, :b, :c, :d] }
+      let(:other_array) { [    :b,         :a] }
+      it { should be_nil }
+    end
+
+    context "([:a, :b, :c, :d, :b], [:a, :c, :b])" do
+      let(:array)       { [:a, :b, :c, :d, :b] }
+      let(:other_array) { [:a,     :c,     :b] }
+      it { should == "10101" }
+    end
+  end
+
   describe ".includes_other_array_items_in_order?" do
     subject { AnsiChameleon::ArrayUtils.includes_other_array_items_in_order?(array, other_array) }
 
