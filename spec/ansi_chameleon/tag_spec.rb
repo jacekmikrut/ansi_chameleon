ba2c2effc12fbafa54a30961ddef70cb2e02da94
Jacek Mikrut <jacekmikrut.software@gmail.com>
2012-08-18 19:46:50 +0200

    Replaced single quotes with double ones in AnsiChameleon::Tag specs (for consistency with other specs).

--- spec/ansi_chameleon/tag_spec.rb
+++ spec/ansi_chameleon/tag_spec.rb
@@ -9,71 +9,71 @@ describe AnsiChameleon::Tag do
   it { should respond_to(:original_string) }
   it { should respond_to(:original_string=) }
 
-  describe 'after initialization' do
-    context 'without parameters' do
+  describe "after initialization" do
+    context "without parameters" do
       its(:name) { should be_nil }
       its(:parent) { should be_nil }
       its(:original_string) { should be_nil }
     end
 
-    context 'with :name value given as string' do
-      subject(:tag) { described_class.new(:name => 'tag-name') }
-      it('should have that value set as string') { tag.name.should == 'tag-name' }
+    context "with :name value given as string" do
+      subject(:tag) { described_class.new(:name => "tag-name") }
+      it("should have that value set as string") { tag.name.should == "tag-name" }
     end
 
-    context 'with :name value given as symbol' do
-      subject(:tag) { described_class.new(:name => :'tag-name') }
-      it('should have that value set as string') { tag.name.should == 'tag-name' }
+    context "with :name value given as symbol" do
+      subject(:tag) { described_class.new(:name => :"tag-name") }
+      it("should have that value set as string") { tag.name.should == "tag-name" }
     end
 
-    context 'with :parent value' do
+    context "with :parent value" do
       subject(:tag) { described_class.new(:parent => parent) }
       let(:parent) { stub(:parent) }
-      it('should have that value set') { tag.parent.should equal(parent) }
+      it("should have that value set") { tag.parent.should equal(parent) }
     end
 
-    context 'with :original_string value' do
+    context "with :original_string value" do
       subject(:tag) { described_class.new(:original_string => original_string) }
-      let(:original_string) { '<tag>' }
-      it('should have that value set') { tag.original_string.should equal(original_string) }
+      let(:original_string) { "<tag>" }
+      it("should have that value set") { tag.original_string.should equal(original_string) }
     end
   end
 
-  describe '#name=' do
+  describe "#name=" do
     subject(:tag)
 
-    context 'when called with a string' do
-      before { tag.name = 'tag-name' }
-      it('should store the value as string') { tag.name.should == 'tag-name' }
+    context "when called with a string" do
+      before { tag.name = "tag-name" }
+      it("should store the value as string") { tag.name.should == "tag-name" }
     end
 
-    context 'when called with a symbol' do
-      before { tag.name = :'tag-name' }
-      it('should store the value as string') { tag.name.should == 'tag-name' }
+    context "when called with a symbol" do
+      before { tag.name = :"tag-name" }
+      it("should store the value as string") { tag.name.should == "tag-name" }
     end
 
   end
 
-  describe '#==' do
-    subject(:tag   ) { described_class.new(:name => 'name', :parent => parent) }
-        let(:other ) { described_class.new(:name => 'name', :parent => parent) }
+  describe "#==" do
+    subject(:tag   ) { described_class.new(:name => "name", :parent => parent) }
+        let(:other ) { described_class.new(:name => "name", :parent => parent) }
         let(:parent) { stub(:parent) }
 
-    context 'when tags have the same names and parents' do
+    context "when tags have the same names and parents" do
       it { expect(tag == other).to be_true }
     end
 
-    context 'when tag names are different' do
-      before { other.name = 'different name' }
+    context "when tag names are different" do
+      before { other.name = "different name" }
       it { expect(tag == other).to be_false }
     end
 
-    context 'when tag parents are different' do
+    context "when tag parents are different" do
       before { other.parent = stub(:different_parent) }
       it { expect(tag == other).to be_false }
     end
 
-    context 'when compared to nil' do
+    context "when compared to nil" do
       it { expect(tag == nil).to be_false }
     end
   end
