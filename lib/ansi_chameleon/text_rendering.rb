b44eed83e5f2f39e7f1e2cdce140fa6b288d4a6e
Jacek Mikrut <jacekmikrut.software@gmail.com>
2012-08-21 19:20:15 +0200

    Now AnsiChameleon::TextRendering uses AnsiChameleon::TextRendering::Stack instead of an array.

--- lib/ansi_chameleon/text_rendering.rb
+++ lib/ansi_chameleon/text_rendering.rb
@@ -9,7 +9,7 @@ module AnsiChameleon
 
     def initialize(style_sheet_handler)
       @style_sheet_handler = style_sheet_handler
-      @stack = []
+      @stack = Stack.new
 
       @current_style = DEFAULT_STYLE.dup
       @current_style = deduce_current_style
@@ -19,17 +19,14 @@ module AnsiChameleon
     end
 
     def push_opening_tag(tag)
-      @stack.push({
-        :tag => tag.tap { |t| t.parent = @stack.last && @stack.last[:tag] },
-        :outer_style => @current_style
-      })
+      @stack.push(:tag => tag, :outer_style => @current_style)
 
       @current_style = deduce_current_style
       @rendered_text << sequence_for(@current_style)
     end
 
     def push_closing_tag(tag)
-      unless @stack.last && @stack.last[:tag].name == tag.name
+      unless @stack.top_tag_name == tag.name
         raise SyntaxError.new("Encountered #{tag.original_string} tag that had not been opened yet")
       end
 
@@ -42,9 +39,9 @@ module AnsiChameleon
     end
 
     def to_s
-      if @stack.any?
-        tag_original_strings = @stack.map { |data| data[:tag].original_string }.join(', ')
-        msg_prefix = @stack.size == 1 ? "Tag #{tag_original_strings} has" : "Tags #{tag_original_strings} have"
+      if @stack.top_tag
+        tag_original_strings = @stack.items.map { |data| data[:tag].original_string }.join(', ')
+        msg_prefix = @stack.items.size == 1 ? "Tag #{tag_original_strings} has" : "Tags #{tag_original_strings} have"
         raise SyntaxError.new(msg_prefix + " been opened but not closed yet")
       end
 
@@ -67,7 +64,7 @@ module AnsiChameleon
 
     def deduce_current_style
       property_names.inject({}) do |style, property_name|
-        property_value = @style_sheet_handler.value_for(@stack.last && @stack.last[:tag], property_name)
+        property_value = @style_sheet_handler.value_for(@stack.top_tag, property_name)
         style[property_name] = [:inherit, nil].include?(property_value) ? @current_style[property_name] : property_value
         style
       end
