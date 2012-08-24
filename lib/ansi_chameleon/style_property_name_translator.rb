7e100de8bc0117c1d296d57bedb3c26215ae60e3
Jacek Mikrut <jacekmikrut.software@gmail.com>
2012-08-24 18:26:24 +0200

    Now AnsiChameleon::StylePropertyNameTranslator also translates String property names.

--- lib/ansi_chameleon/style_property_name_translator.rb
+++ lib/ansi_chameleon/style_property_name_translator.rb
@@ -2,7 +2,7 @@ module AnsiChameleon
   module StylePropertyNameTranslator
 
     def self.translate(property_name)
-      case property_name
+      case property_name.to_sym
 
       when :background_color, :background, :bg_color
         :background_color
--- spec/ansi_chameleon/style_property_name_translator_spec.rb
+++ spec/ansi_chameleon/style_property_name_translator_spec.rb
@@ -6,27 +6,55 @@ describe AnsiChameleon::StylePropertyNameTranslator do
     it { subject.translate(:background_color).should == :background_color }
   end
 
+  describe ".translate('background_color')" do
+    it { subject.translate('background_color').should == :background_color }
+  end
+
   describe ".translate(:background)" do
     it { subject.translate(:background).should       == :background_color }
+  end
+
+  describe ".translate('background')" do
+    it { subject.translate('background').should       == :background_color }
   end
 
   describe ".translate(:bg_color)" do
     it { subject.translate(:bg_color).should         == :background_color }
   end
 
+  describe ".translate('bg_color')" do
+    it { subject.translate('bg_color').should         == :background_color }
+  end
+
   describe ".translate(:foreground_color)" do
     it { subject.translate(:foreground_color).should == :foreground_color }
+  end
+
+  describe ".translate('foreground_color')" do
+    it { subject.translate('foreground_color').should == :foreground_color }
   end
 
   describe ".translate(:foreground)" do
     it { subject.translate(:foreground).should       == :foreground_color }
   end
 
+  describe ".translate('foreground')" do
+    it { subject.translate('foreground').should       == :foreground_color }
+  end
+
   describe ".translate(:fg_color)" do
     it { subject.translate(:fg_color).should         == :foreground_color }
   end
 
+  describe ".translate('fg_color')" do
+    it { subject.translate('fg_color').should         == :foreground_color }
+  end
+
   describe ".translate(:effect)" do
     it { subject.translate(:effect).should           == :effect }
+  end
+
+  describe ".translate('effect')" do
+    it { subject.translate('effect').should           == :effect }
   end
 end
