cedaa2eaff3f26adb62a4b7ca8e6bd72cfb52aed
Jacek Mikrut <jacekmikrut.software@gmail.com>
2012-08-24 12:52:22 +0200

    Integration specs now test tags with attributes.

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -4,23 +4,23 @@ describe "Single use" do
 
   it "should properly render the output" do
     AnsiChameleon.render(
-      "There are <number>3</number> oranges on the table.",
-      "number" => { :fg_color => :blue }
+      '<p class="poetry">There are <number>3</number> oranges on the table.</p>',
+      "p.poetry" => { :effect => :bright }, "number" => { :fg_color => :blue }
     )
-    .should == "\e[0m\e[37m\e[40mThere are \e[0m\e[34m\e[40m3\e[0m\e[37m\e[40m oranges on the table.\e[0m"
+    .should == "\e[0m\e[37m\e[40m\e[1m\e[37m\e[40mThere are \e[1m\e[34m\e[40m3\e[1m\e[37m\e[40m oranges on the table.\e[0m\e[37m\e[40m\e[0m"
   end
 end
 
 describe "Multiple use" do
 
   it "should properly render the output" do
-    text_renderer = AnsiChameleon::TextRenderer.new("number" => { :fg_color => :blue })
+    text_renderer = AnsiChameleon::TextRenderer.new("p.poetry" => { :effect => :bright }, "number" => { :fg_color => :blue })
 
-    text_renderer.render("There are <number>2</number> oranges on the table.")
-    .should == "\e[0m\e[37m\e[40mThere are \e[0m\e[34m\e[40m2\e[0m\e[37m\e[40m oranges on the table.\e[0m"
+    text_renderer.render('<p class="poetry">There are <number>2</number> oranges on the table.</p>')
+    .should == "\e[0m\e[37m\e[40m\e[1m\e[37m\e[40mThere are \e[1m\e[34m\e[40m2\e[1m\e[37m\e[40m oranges on the table.\e[0m\e[37m\e[40m\e[0m"
 
-    text_renderer.render("Now, there is only <number>1</number> orange.")
-    .should == "\e[0m\e[37m\e[40mNow, there is only \e[0m\e[34m\e[40m1\e[0m\e[37m\e[40m orange.\e[0m"
+    text_renderer.render('<p class="poetry">Now, there is only <number>1</number> orange.</p>')
+    .should == "\e[0m\e[37m\e[40m\e[1m\e[37m\e[40mNow, there is only \e[1m\e[34m\e[40m1\e[1m\e[37m\e[40m orange.\e[0m\e[37m\e[40m\e[0m"
   end
 end
 
