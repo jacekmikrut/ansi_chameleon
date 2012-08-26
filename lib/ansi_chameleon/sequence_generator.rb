7c656dd458e3d5f22e25bf67dbb6f362076fbaf4
Jacek Mikrut <jacekmikrut.software@gmail.com>
2012-08-26 11:31:12 +0200

    Refactored AnsiChameleon::SequenceGenerator class.

--- lib/ansi_chameleon/sequence_generator.rb
+++ lib/ansi_chameleon/sequence_generator.rb
@@ -7,6 +7,8 @@ module AnsiChameleon
     COLORS = [:black, :red, :green, :yellow, :blue, :magenta, :cyan, :white]
     EFFECTS = { :none => 0, :bright => 1, :underline => 4, :blink => 5, :reverse => 7 }
 
+    NUMBER_FROM_0_TO_255 = proc { |value| value.to_s =~ /^\d+$/ && (0..255).member?(value.to_i) }
+
     def self.generate(style)
       "\033[0m" + style.map { |name, value| send("#{name}_sequence", value) }.join
     end
@@ -22,7 +24,7 @@ module AnsiChameleon
     end
 
     def self.foreground_color_sequence(value)
-      if number_from_0_to_255?(value)
+      if NUMBER_FROM_0_TO_255.call(value)
         "\033[38;5;#{value}m"
 
       elsif COLORS.include?(value.to_s.to_sym)
@@ -35,7 +37,7 @@ module AnsiChameleon
     end
 
     def self.background_color_sequence(value)
-      if number_from_0_to_255?(value)
+      if NUMBER_FROM_0_TO_255.call(value)
         "\033[48;5;#{value}m"
 
       elsif COLORS.include?(value.to_s.to_sym)
@@ -45,10 +47,6 @@ module AnsiChameleon
         raise InvalidColorValueError.new("Invalid background color value #{value.inspect}")
 
       end
-    end
-
-    def self.number_from_0_to_255?(value)
-      value.to_s =~ /^\d+$/ && (0..255).member?(value.to_i)
     end
   end
 end
